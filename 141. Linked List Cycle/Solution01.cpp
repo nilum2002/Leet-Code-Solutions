b39167fdcb26f7fc0afe7c9e31678139f9bffc60
Nilum <sahithyanilum@gmail.com>
2025-04-13 19:05:04 +0530

    Add solutions for "Linked List Cycle" problem using two approaches

---
+++ 141. Linked List Cycle/Solution01.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct ListNode {
+         int val;
+        ListNode *next;
+        ListNode(int x) : val(x), next(NULL) {} 
+};
+
+
+bool hasCycle(ListNode *head) {
+
+    set<ListNode*> hash_set ;
+
+    ListNode* curr = head;
+    while (curr){
+        if (hash_set.find(curr) != hash_set.end()){
+            return true;
+            // break;
+        }else{
+            hash_set.insert(curr);
+        }
+        curr = curr->next;
+    }
+    return false;
+    
+}
