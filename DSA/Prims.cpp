55d72471234f5acb500541b0abbe1ee70c1853d1
Nilum <sachithyanilum@gmail.com>
2025-06-01 15:47:09 +0530

    Included prims algorithm

---
+++ DSA/Prims.cpp
@@ -0,0 +1,128 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+int prims(int n, vector<vector<int>> edges, int start) {
+    vector<vector<pair<int, int>>> adj(n);
+    for (auto &e : edges) {
+        int u = e[0] - 1, v = e[1] - 1, w = e[2];
+        adj[u].push_back({v, w});
+        adj[v].push_back({u, w});
+    }
+
+    vector<int> key(n, INT_MAX);
+    vector<bool> inMST(n, false);
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> pq;
+
+    key[start - 1] = 0;
+    pq.push({0, start - 1});
+
+    int cost = 0;
+
+    while (!pq.empty()) {
+        int u = pq.top().second;
+        pq.pop();
+
+        if (inMST[u]) continue;
+
+        inMST[u] = true;
+        cost += key[u];
+
+        for (auto &[v, weight] : adj[u]) {
+            if (!inMST[v] && weight < key[v]) {
+                key[v] = weight;
+                pq.push({key[v], v});
+            }
+        }
+    }
+
+    return cost;
+}
+
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string first_multiple_input_temp;
+    getline(cin, first_multiple_input_temp);
+
+    vector<string> first_multiple_input = split(rtrim(first_multiple_input_temp));
+
+    int n = stoi(first_multiple_input[0]);
+
+    int m = stoi(first_multiple_input[1]);
+
+    vector<vector<int>> edges(m);
+
+    for (int i = 0; i < m; i++) {
+        edges[i].resize(3);
+
+        string edges_row_temp_temp;
+        getline(cin, edges_row_temp_temp);
+
+        vector<string> edges_row_temp = split(rtrim(edges_row_temp_temp));
+
+        for (int j = 0; j < 3; j++) {
+            int edges_row_item = stoi(edges_row_temp[j]);
+
+            edges[i][j] = edges_row_item;
+        }
+    }
+
+    string start_temp;
+    getline(cin, start_temp);
+
+    int start = stoi(ltrim(rtrim(start_temp)));
+
+    int result = prims(n, edges, start);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
